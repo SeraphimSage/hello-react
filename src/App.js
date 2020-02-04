d049b3ab6d18c67b364ad6fcec599b92d7228319
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-02-04 12:46:40 -0700

    Completes project.

--- src/App.js
+++ src/App.js
@@ -2,28 +2,21 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function Text() {
-  return <span>Kenneth</span>;
+function Text(props) {
+	return <span style={{ color: props.color }}>{props.value}</span>;
 }
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, <Text color ="green" value="Kenneth"/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Hello, <Text color="green" value="Kenneth" />
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
